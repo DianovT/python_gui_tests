4689764f7322d050f0ccf0e2d285c7417b3fc482
dianov <Dianov@inventive.ru>
2021-08-12 12:34:47 +0300

    Правки по заданию №23

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -3,11 +3,12 @@ import random
 
 def test_delete_group(app):
     if len(app.groups.get_group_list()) == 1:
-        app.groups.add_new_group("my group")
+        app.groups.add_new_group("my group1")
     old_list = app.groups.get_group_list()
     group = random.choice(old_list)
     index = old_list.index(group)
     app.groups.delete_group(index)
     new_list = app.groups.get_group_list()
-    old_list.remove(group)
+    assert len(old_list) - 1 == len(new_list)
+    old_list[index:index+1] = []
     assert sorted(old_list) == sorted(new_list)
